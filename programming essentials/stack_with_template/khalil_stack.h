6c88c5ce4fdd5fac66dc35faf57cf0edc5cbc448
Mohamed Khalil <khm40005000@outlook.com>
2025-12-06 16:34:39 +0200

    finished the stack class with templates task

--- programming essentials/stack_with_template/khalil_stack.h
+++ programming essentials/stack_with_template/khalil_stack.h
@@ -5,23 +5,103 @@ using namespace std;
 #include <stdexcept>
 #include <iostream>
 
+template <typename T>
 class stack_clone
 {
 private:
     int size, curr;
-    int *arr;
+    T *arr;
+
 public:
-    stack_clone();
-    stack_clone(const stack_clone& st);
+    stack_clone()
+    {
+        this->arr = new T[5];
+        this->curr = 0;
+        this->size = 5;
+    }
+    stack_clone(const stack_clone &other)
+    {
+        if (this == &other)
+            return;
+        this->curr = other.curr;
+        this->size = other.size;
+        this->arr = new T[other.size];
+        for (int i = 0; i < other.curr; i++)
+        {
+            this->arr[i] = other.arr[i];
+        }
+    }
 
-    stack_clone& operator=(const stack_clone& st);
-    void push(int val);
-    int pop();
-    bool empty();
-    int top();
-    int getSize();
+    stack_clone &operator=(const stack_clone &other)
+    {
+        if (this == &other)
+            return *this;
+        if (this->size < other.size)
+        {
+            this->size = other.size;
+            delete[] this->arr;
+            this->arr = new T[other.size];
+        }
+        this->curr = other.curr;
+        for (int i = 0; i < other.curr; i++)
+        {
+            this->arr[i] = other.arr[i];
+        }
+        return *this; // to enable chaining st1=st2=st3
+    }
+    void push(T val)
+    {
+        if (this->curr < size)
+        {
+            this->arr[curr] = val;
+            this->curr++;
+        }
+        else
+        {
+            this->size = this->size * 2;
+            T *new_ptr = new T[this->size];
+            for (int i = 0; i < this->size / 2; i++)
+                new_ptr[i] = arr[i];
+            new_ptr[curr] = val;
+            delete[] arr;
+            arr = new_ptr;
+            curr++;
+        }
+    }
+    T pop()
+    {
+        if (this->curr > 0)
+        {
+            T val = this->arr[curr - 1];
+            this->curr--;
+            return val;
+        }
+        else
+        {
+            throw out_of_range("Cannot pop from an empty stack");
+        }
+    }
+    bool empty()
+    {
+        return curr == 0;
+    }
+    T top()
+    {
+        if (this->empty())
+        {
+            throw out_of_range("Stack is empty");
+        }
+        return this->arr[curr - 1];
+    }
+    int getSize()
+    {
+        return this->size;
+    }
 
-    ~stack_clone();
+    ~stack_clone()
+    {
+        delete[] this->arr;
+    }
 };
 
 #endif
--- programming essentials/stack_with_template/main.cpp
+++ programming essentials/stack_with_template/main.cpp
@@ -1,21 +1,22 @@
 #include "khalil_stack.h"
 
 int main(){
-    stack_clone st = stack_clone();
-    st.push(2);
-    st.push(3);
-    st.push(1);
-    st.push(3);
-    st.push(1);
-    st.push(1);
-    st.push(1);
-    stack_clone st2 = stack_clone();
+    stack_clone st = stack_clone<string>();
+    st.push("fsdf");
+    st.push("fsdfs");
+    st.push("fsdf");
+    st.push("fdv");
+    st.push("bncvs");
+    st.push("bger");
+    st.push("bsfdg");
+    stack_clone st2 = stack_clone<string>();
     cout << st2.getSize()<<'\n';
     st2 = st;
     cout << st2.getSize()<<'\n';
-    while(!st2.empty()) cout << st2.pop();
+    while(!st2.empty()) cout << st2.pop() << endl;
     cout << endl;
-    while(!st.empty()) cout << st.pop();
+    while(!st.empty()) cout << st.pop() << endl;
+    cout << endl;
 
     return 0;
 }
